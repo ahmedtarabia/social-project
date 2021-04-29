7bc2d829330a2c88cb477992d9b652f75468a516
Pamela Caguana <pamelacaguana@gmail.com>
2021-04-29 17:12:53 -0600

    deleted db connection and added MH comments

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const app = express();
+//installed body parser; when I did this got a message that new major version of npm available, released like 1hr ago: https://github.com/npm/cli/releases/tag/v7.11.2        MH
+const bodyParser = require('body-parser');
+//installed mongodb 
+const MongoClient = require('mongodb').MongoClient;
 const port = 2121;
 const dotenv = require('dotenv')
+
+//connect to MongoDB with connection string MH
+MongoClient.connect(`${MONGO_URI}`,{
+  useUnifiedTopology: true})
+    .then(client => {
+      console.log('Connected to Database')
+      //changing database name MH
+      const db = client.db('social-platform-project')
+      const quotesCollection = db.collection('quotes')
+    })
+    .catch(err => console.log(error))
 
 //set up ejs for views
 app.set("view engine", "ejs");
@@ -10,6 +25,7 @@ app.set("view engine", "ejs");
 app.use(express.static("public"))
 
 //parsing
+//express lets us use middleware (like bodyParser) with 'use' method MH
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
@@ -18,8 +34,17 @@ app.use(express.json())
 app.use('/', require('./routes/index'))
 
 //move to posts.js in routes folder
+//maybe change this to be a post or login form? MH
 app.post('/posts', (req, res) => {
   console.log('Hellooooooooooooooooo!')
+  //adding this to see values from post 
+  console.log(req.body)
+  //adding items to MongoDB collection, not sure how to get this to work MH
+  quotesCollection.insertOne(req.body)
+    .then(result => {
+      console.log(result)
+    })
+    .catch(error => console.log(error))
 })
 
 app.listen(port, function() {
